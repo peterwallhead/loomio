e7e6e4c4287f73483c6c5da2c86d8fed263c5783
James Kiesel <james.kiesel@gmail.com>
2016-01-10 23:43:03 +1300

    Keep on hackin in the free world

---
+++ plugins/position_specific/plugin.rb
@@ -0,0 +1,41 @@
+module Plugins
+  module LoomioWebhooks
+    class Plugin < Plugins::Base
+
+      setup! :position_specific do |plugin|
+        plugin.enabled = true
+
+        plugin.use_component :position_fields, outlet: :after_proposal_form
+        plugin.use_component :position_voting, outlet: :after_position_buttons
+
+        plugin.extend_class PermittedParams do
+          def motion_attributes
+            proposal_attributes | [:agree_description, :abstain_description, :disagree_description, :block_description]
+          end
+        end
+
+        plugin.extend_class Motion do
+          attr_accessor :agree_description, :abstain_description, :disagree_description, :block_description
+
+          def position_descriptions
+            {
+              agree_description:    self.agree_description,
+              abstain_description:  self.abstain_description,
+              disagree_description: self.disagree_description,
+              block_description:    self.block_description
+            }
+          end
+
+          def save_position_descriptions
+            specifics.find_or_initialize_by(key: :position_descriptions).update(value: position_descriptions)
+          end
+        end
+
+        plugin.use_events do |event_bus|
+          event_bus.listen('motion_create', 'motion_update') { |motion| motion.save_position_descriptions }
+        end
+
+      end
+    end
+  end
+end
